5096e2f512f83902214f7c7df26689577f361a6e
gianlucm76 <gianluca.mardente@gmail.com>
2023-06-04 14:55:12 +0200

    AddonConstraint controller
    Watches for:
    1. referenced ConfigMap/Secret and Flux sources
    2. get content (openapi validations) in all referenced sources
    3. update status section with current openapi validations
    
    It also watches for Cluster/SveltosCluster instances:
    1. for each matching cluster, add a label to AddonConstraint instance
    
    This PR includes:
    1. code
    2. unit tests
    3. functional verification using a Kind cluster

---
+++ controllers/addonconstraint_predicates.go
@@ -0,0 +1,478 @@
+/*
+Copyright 2023. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"fmt"
+	"reflect"
+
+	sourcev1 "github.com/fluxcd/source-controller/api/v1"
+	sourcev1b2 "github.com/fluxcd/source-controller/api/v1beta2"
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	libsveltosv1alpha1 "github.com/projectsveltos/libsveltos/api/v1alpha1"
+	logs "github.com/projectsveltos/libsveltos/lib/logsettings"
+)
+
+// ConfigMapPredicates predicates for ConfigMaps. AddonConstraintReconciler watches ConfigMap events
+// and react to those by reconciling itself based on following predicates
+func ConfigMapPredicates(logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			newConfigMap := e.ObjectNew.(*corev1.ConfigMap)
+			oldConfigMap := e.ObjectOld.(*corev1.ConfigMap)
+			log := logger.WithValues("predicate", "updateEvent",
+				"configmap", newConfigMap.Name,
+			)
+
+			if oldConfigMap == nil {
+				log.V(logs.LogVerbose).Info("Old ConfigMap is nil. Reconcile AddonConstraints.")
+				return true
+			}
+
+			if !reflect.DeepEqual(oldConfigMap.Data, newConfigMap.Data) {
+				log.V(logs.LogVerbose).Info(
+					"ConfigMap Data changed. Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+
+			if !reflect.DeepEqual(oldConfigMap.BinaryData, newConfigMap.BinaryData) {
+				log.V(logs.LogVerbose).Info(
+					"ConfigMap BinaryData changed. Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogVerbose).Info(
+				"ConfigMap did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			return CreateFuncTrue(e, logger)
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return DeleteFuncTrue(e, logger)
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			return GenericFuncFalse(e, logger)
+		},
+	}
+}
+
+// SecretPredicates predicates for Secrets. AddonConstraintReconciler watches Secret events
+// and react to those by reconciling itself based on following predicates
+func SecretPredicates(logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			newSecret := e.ObjectNew.(*corev1.Secret)
+			oldSecret := e.ObjectOld.(*corev1.Secret)
+			log := logger.WithValues("predicate", "updateEvent",
+				"secret", newSecret.Name,
+			)
+
+			if oldSecret == nil {
+				log.V(logs.LogVerbose).Info("Old Secret is nil. Reconcile AddonConstraints.")
+				return true
+			}
+
+			if !reflect.DeepEqual(oldSecret.Data, newSecret.Data) {
+				log.V(logs.LogVerbose).Info(
+					"Secret Data changed. Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogVerbose).Info(
+				"Secret did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			return CreateFuncTrue(e, logger)
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return DeleteFuncTrue(e, logger)
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			return GenericFuncFalse(e, logger)
+		},
+	}
+}
+
+var (
+	CreateFuncTrue = func(e event.CreateEvent, logger logr.Logger) bool {
+		log := logger.WithValues("predicate", "createEvent",
+			e.Object.GetObjectKind(), e.Object.GetName(),
+		)
+
+		log.V(logs.LogVerbose).Info(fmt.Sprintf(
+			"%s did match expected conditions.  Will attempt to reconcile associated AddonConstraints.",
+			e.Object.GetObjectKind()))
+		return true
+	}
+
+	DeleteFuncTrue = func(e event.DeleteEvent, logger logr.Logger) bool {
+		log := logger.WithValues("predicate", "deleteEvent",
+			e.Object.GetObjectKind(), e.Object.GetName(),
+		)
+		log.V(logs.LogVerbose).Info(fmt.Sprintf(
+			"%s did match expected conditions.  Will attempt to reconcile associated AddonConstraints.",
+			e.Object.GetObjectKind()))
+		return true
+	}
+
+	GenericFuncFalse = func(e event.GenericEvent, logger logr.Logger) bool {
+		log := logger.WithValues("predicate", "genericEvent",
+			e.Object.GetObjectKind(), e.Object.GetName(),
+		)
+		log.V(logs.LogVerbose).Info(fmt.Sprintf(
+			"%s did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.",
+			e.Object.GetObjectKind()))
+		return false
+	}
+)
+
+// FluxSourcePredicates predicates for GitRepository/OCIRepository/Bucket.
+// AddonConstraintReconciler watches GitRepository/OCIRepository/Bucket events and
+// react to those by reconciling itself based on following predicates
+func FluxSourcePredicates(s *runtime.Scheme, logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			log := logger.WithValues("predicate", "updateEvent",
+				"namespace", e.ObjectNew.GetNamespace(),
+				"source", e.ObjectNew.GetName(),
+			)
+
+			if hasArtifactChanged(s, e) {
+				log.V(logs.LogInfo).Info(
+					"Source artifact has changed.  Will attempt to reconcile associated AddonConstraints.")
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogInfo).Info(
+				"GitRepository did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			log := logger.WithValues("predicate", "createEvent",
+				"namespace", e.Object.GetNamespace(),
+				"source", e.Object.GetName(),
+			)
+
+			log.V(logs.LogVerbose).Info(
+				"Source did match expected conditions.  Will attempt to reconcile associated AddonConstraints.")
+			return true
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log := logger.WithValues("predicate", "deleteEvent",
+				"namespace", e.Object.GetNamespace(),
+				"source", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Source deleted.  Will attempt to reconcile associated AddonConstraints.")
+			return true
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			log := logger.WithValues("predicate", "genericEvent",
+				"namespace", e.Object.GetNamespace(),
+				"source", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Source did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+	}
+}
+
+func hasArtifactChanged(s *runtime.Scheme, e event.UpdateEvent) bool {
+	object := e.ObjectNew
+
+	addTypeInformationToObject(s, object)
+
+	switch object.GetObjectKind().GroupVersionKind().Kind {
+	case sourcev1.GitRepositoryKind:
+		newGitRepo := e.ObjectNew.(*sourcev1.GitRepository)
+		oldGitRepo := e.ObjectOld.(*sourcev1.GitRepository)
+		if oldGitRepo == nil ||
+			!isArtifactSame(oldGitRepo.Status.Artifact, newGitRepo.Status.Artifact) {
+
+			return true
+		}
+	case sourcev1b2.BucketKind:
+		newBucket := e.ObjectNew.(*sourcev1b2.Bucket)
+		oldBucket := e.ObjectOld.(*sourcev1b2.Bucket)
+		if oldBucket == nil ||
+			!isArtifactSame(oldBucket.Status.Artifact, newBucket.Status.Artifact) {
+
+			return true
+		}
+	case sourcev1b2.OCIRepositoryKind:
+		newOCIRepo := e.ObjectNew.(*sourcev1b2.OCIRepository)
+		oldOCIRepo := e.ObjectOld.(*sourcev1b2.OCIRepository)
+		if oldOCIRepo == nil ||
+			!isArtifactSame(oldOCIRepo.Status.Artifact, newOCIRepo.Status.Artifact) {
+
+			return true
+		}
+	}
+
+	return false
+}
+
+func isArtifactSame(oldArtifact, newArtifact *sourcev1.Artifact) bool {
+	if oldArtifact == nil && newArtifact != nil {
+		return false
+	}
+	if oldArtifact != nil && newArtifact == nil {
+		return false
+	}
+	return reflect.DeepEqual(oldArtifact, newArtifact)
+}
+
+// SveltosClusterPredicates predicates for sveltos Cluster. AddonConstraintReconciler watches sveltos Cluster events
+// and react to those by reconciling itself based on following predicates
+func SveltosClusterPredicates(logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			newCluster := e.ObjectNew.(*libsveltosv1alpha1.SveltosCluster)
+			oldCluster := e.ObjectOld.(*libsveltosv1alpha1.SveltosCluster)
+			log := logger.WithValues("predicate", "updateEvent",
+				"namespace", newCluster.Namespace,
+				"cluster", newCluster.Name,
+			)
+
+			if oldCluster == nil {
+				log.V(logs.LogVerbose).Info("Old Cluster is nil. Reconcile AddonConstraint")
+				return true
+			}
+
+			// return true if Cluster.Spec.Paused has changed from true to false
+			if oldCluster.Spec.Paused && !newCluster.Spec.Paused {
+				log.V(logs.LogVerbose).Info(
+					"Cluster was unpaused. Will attempt to reconcile associated AddonConstraints.")
+				return true
+			}
+
+			if !oldCluster.Status.Ready && newCluster.Status.Ready {
+				log.V(logs.LogVerbose).Info(
+					"Cluster was not ready. Will attempt to reconcile associated AddonConstraints.")
+				return true
+			}
+
+			if !reflect.DeepEqual(oldCluster.Labels, newCluster.Labels) {
+				log.V(logs.LogVerbose).Info(
+					"Cluster labels changed. Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			cluster := e.Object.(*libsveltosv1alpha1.SveltosCluster)
+			log := logger.WithValues("predicate", "createEvent",
+				"namespace", cluster.Namespace,
+				"cluster", cluster.Name,
+			)
+
+			// Only need to trigger a reconcile if the Cluster.Spec.Paused is false
+			if !cluster.Spec.Paused {
+				log.V(logs.LogVerbose).Info(
+					"Cluster is not paused.  Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log := logger.WithValues("predicate", "deleteEvent",
+				"namespace", e.Object.GetNamespace(),
+				"cluster", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Cluster deleted.  Will attempt to reconcile associated AddonConstraints.")
+			return true
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			log := logger.WithValues("predicate", "genericEvent",
+				"namespace", e.Object.GetNamespace(),
+				"cluster", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+	}
+}
+
+// ClusterPredicates predicates for v1Cluster. AddonConstraintReconciler watches v1Cluster events
+// and react to those by reconciling itself based on following predicates
+func ClusterPredicates(logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			newCluster := e.ObjectNew.(*clusterv1.Cluster)
+			oldCluster := e.ObjectOld.(*clusterv1.Cluster)
+			log := logger.WithValues("predicate", "updateEvent",
+				"namespace", newCluster.Namespace,
+				"cluster", newCluster.Name,
+			)
+
+			if oldCluster == nil {
+				log.V(logs.LogVerbose).Info("Old Cluster is nil. Reconcile AddonConstraint")
+				return true
+			}
+
+			// return true if Cluster.Spec.Paused has changed from true to false
+			if oldCluster.Spec.Paused && !newCluster.Spec.Paused {
+				log.V(logs.LogVerbose).Info(
+					"Cluster was unpaused. Will attempt to reconcile associated AddonConstraints.")
+				return true
+			}
+
+			if !reflect.DeepEqual(oldCluster.Labels, newCluster.Labels) {
+				log.V(logs.LogVerbose).Info(
+					"Cluster labels changed. Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			cluster := e.Object.(*clusterv1.Cluster)
+			log := logger.WithValues("predicate", "createEvent",
+				"namespace", cluster.Namespace,
+				"cluster", cluster.Name,
+			)
+
+			// Only need to trigger a reconcile if the Cluster.Spec.Paused is false
+			if !cluster.Spec.Paused {
+				log.V(logs.LogVerbose).Info(
+					"Cluster is not paused.  Will attempt to reconcile associated AddonConstraints.",
+				)
+				return true
+			}
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log := logger.WithValues("predicate", "deleteEvent",
+				"namespace", e.Object.GetNamespace(),
+				"cluster", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Cluster deleted.  Will attempt to reconcile associated AddonConstraints.")
+			return true
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			log := logger.WithValues("predicate", "genericEvent",
+				"namespace", e.Object.GetNamespace(),
+				"cluster", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Cluster did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+	}
+}
+
+// MachinePredicates predicates for v1Machine. AddonConstraintReconciler watches v1Machine events
+// and react to those by reconciling itself based on following predicates
+func MachinePredicates(logger logr.Logger) predicate.Funcs {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			newMachine := e.ObjectNew.(*clusterv1.Machine)
+			oldMachine := e.ObjectOld.(*clusterv1.Machine)
+			log := logger.WithValues("predicate", "updateEvent",
+				"namespace", newMachine.Namespace,
+				"machine", newMachine.Name,
+			)
+
+			if newMachine.Status.GetTypedPhase() != clusterv1.MachinePhaseRunning {
+				return false
+			}
+
+			if oldMachine == nil {
+				log.V(logs.LogVerbose).Info("Old Machine is nil. Reconcile AddonConstraint")
+				return true
+			}
+
+			// return true if Machine.Status.Phase has changed from not running to running
+			if oldMachine.Status.GetTypedPhase() != newMachine.Status.GetTypedPhase() {
+				log.V(logs.LogVerbose).Info(
+					"Machine was not in Running Phase. Will attempt to reconcile associated AddonConstraints.")
+				return true
+			}
+
+			// otherwise, return false
+			log.V(logs.LogVerbose).Info(
+				"Machine did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			machine := e.Object.(*clusterv1.Machine)
+			log := logger.WithValues("predicate", "createEvent",
+				"namespace", machine.Namespace,
+				"machine", machine.Name,
+			)
+
+			// Only need to trigger a reconcile if the Machine.Status.Phase is Running
+			if machine.Status.GetTypedPhase() == clusterv1.MachinePhaseRunning {
+				return true
+			}
+
+			log.V(logs.LogVerbose).Info(
+				"Machine did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			log := logger.WithValues("predicate", "deleteEvent",
+				"namespace", e.Object.GetNamespace(),
+				"machine", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Machine did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+		GenericFunc: func(e event.GenericEvent) bool {
+			log := logger.WithValues("predicate", "genericEvent",
+				"namespace", e.Object.GetNamespace(),
+				"machine", e.Object.GetName(),
+			)
+			log.V(logs.LogVerbose).Info(
+				"Machine did not match expected conditions.  Will not attempt to reconcile associated AddonConstraints.")
+			return false
+		},
+	}
+}
---
+++ controllers/utils_test.go
@@ -0,0 +1,58 @@
+/*
+Copyright 2023. projectsveltos.io. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers_test
+
+import (
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/klog/v2/klogr"
+
+	"github.com/projectsveltos/addon-constraint-controller/controllers"
+)
+
+var _ = Describe("Utilities", func() {
+
+	It("collectContentOfSecret returns openapi policy", func() {
+		// Create a temporary directory for testing
+		dir, err := os.MkdirTemp("", "testdir")
+		Expect(err).To(BeNil())
+		defer os.RemoveAll(dir)
+
+		// Create a few test files with content
+		files := []struct {
+			name    string
+			content string
+		}{
+			{"depl.txt", deplReplicaSpec},
+			{"name.txt", nameSpec},
+		}
+
+		for _, f := range files {
+			filePath := filepath.Join(dir, f.name)
+			err = os.WriteFile(filePath, []byte(f.content), 0644)
+			Expect(err).To(BeNil())
+		}
+
+		var policies map[string]string
+		policies, err = controllers.WalkDir(dir, klogr.New())
+		Expect(err).To(BeNil())
+		Expect(len(policies)).To(Equal(2))
+	})
+})
